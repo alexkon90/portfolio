de3be347fe552478420f5887e8334e633ad01589
Alex <konyulichalex@gmail.com>
2026-03-08 21:58:34 +0200

    auto fixed

--- works/auto/src/scripts/main.js
+++ works/auto/src/scripts/main.js
@@ -1,34 +1,60 @@
-function initMobileMenu() {
-    // Mobile menu
-    const menu = document.querySelectorAll('[data-menu]')
-    if (!menu.length) return
+//function initMobileMenu() {
+//    // Mobile menu
+//    const menu = document.querySelectorAll('[data-menu]')
+//    if (!menu.length) return
 
-}
+//}
 
-function initTabs() {
-    // Tabs
-    const tabs = document.querySelectorAll('[data-tabs]')
-    if (!tabs.length) return
+//function initModal() {
+//    // Modal
+//    const modal = document.querySelector('[data-modal]')
+//    if (!modal) return
 
-}
+//}
 
-function initAccordion() {
-    // Accordion
-    const accordion = document.querySelector('[data-accordion]')
-    if (!accordion) return
 
-}
+function initAccordion(selector, allowMultiple = false){
+	const items = document.querySelectorAll(selector);
 
-function initModal() {
-    // Modal
-    const modal = document.querySelector('[data-modal]')
-    if (!modal) return
+	items.forEach(item => {
+		const button = item.querySelector('.accordeon_caption');
+		const content = item.querySelector('.accordeon_content');
 
+		if(item.classList.contains('open')){
+			content.style.height = content.scrollHeight + 'px';
+		}
+		button.addEventListener('click', () => {
+			const isOpen = item.classList.contains('open');
+			if(!allowMultiple){
+				items.forEach(el => {
+					if(el !== item && el.classList.contains('open')){
+						const c = el.querySelector('.accordeon_content');
+						c.style.height = c.scrollHeight + 'px';
+						requestAnimationFrame(()=>{
+							c.style.height = '0px';
+						});
+						el.classList.remove('open');
+					}
+				});
+			}
+			if(isOpen){
+				content.style.height = content.scrollHeight + 'px';
+				requestAnimationFrame(()=>{
+					content.style.height = '0px';
+				});
+				item.classList.remove('open');
+			}else{
+				item.classList.add('open');
+				content.style.height = '0px';
+				requestAnimationFrame(()=>{
+					content.style.height = content.scrollHeight + 'px';
+				});
+			}
+		});
+	});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    initMobileMenu();
-    initTabs();
-    initAccordion();
-    initModal();
+    initAccordion('.benefits-faq-item', false); // Если надо несколько открытых - ставим true
+    initAccordion('.address-acrd-item', false); // Если надо несколько открытых - ставим true
 });
