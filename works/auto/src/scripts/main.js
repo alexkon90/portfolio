d0221456b33f17ea599247d00f307d18b7fc40b4
Alex <konyulichalex@gmail.com>
2026-03-09 16:00:04 +0200

    auto mobile 6

--- works/auto/src/scripts/main.js
+++ works/auto/src/scripts/main.js
@@ -1,60 +1,59 @@
-// Smooth scroll
-document.querySelectorAll('.link_scroll').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scroll
+    document.querySelectorAll('.link_scroll').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        const id = this.getAttribute('href');
-        const target = document.querySelector(id);
+            const id = this.getAttribute('href');
+            const target = document.querySelector(id);
 
-        target.scrollIntoView({
-            behavior: "smooth"
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
         });
     });
-});
 
-// Accordeon
-function initAccordion(selector, allowMultiple = false){
-	const items = document.querySelectorAll(selector);
+    // Accordeon
+    function initAccordion(selector, allowMultiple = false){
+        const items = document.querySelectorAll(selector);
 
-	items.forEach(item => {
-		const button = item.querySelector('.accordeon_caption');
-		const content = item.querySelector('.accordeon_content');
+        items.forEach(item => {
+            const button = item.querySelector('.accordeon_caption');
+            const content = item.querySelector('.accordeon_content');
 
-		if(item.classList.contains('open')){
-			content.style.height = content.scrollHeight + 'px';
-		}
-		button.addEventListener('click', () => {
-			const isOpen = item.classList.contains('open');
-			if(!allowMultiple){
-				items.forEach(el => {
-					if(el !== item && el.classList.contains('open')){
-						const c = el.querySelector('.accordeon_content');
-						c.style.height = c.scrollHeight + 'px';
-						requestAnimationFrame(()=>{
-							c.style.height = '0px';
-						});
-						el.classList.remove('open');
-					}
-				});
-			}
-			if(isOpen){
-				content.style.height = content.scrollHeight + 'px';
-				requestAnimationFrame(()=>{
-					content.style.height = '0px';
-				});
-				item.classList.remove('open');
-			}else{
-				item.classList.add('open');
-				content.style.height = '0px';
-				requestAnimationFrame(()=>{
-					content.style.height = content.scrollHeight + 'px';
-				});
-			}
-		});
-	});
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+            if(item.classList.contains('open')){
+                content.style.height = content.scrollHeight + 'px';
+            }
+            button.addEventListener('click', () => {
+                const isOpen = item.classList.contains('open');
+                if(!allowMultiple){
+                    items.forEach(el => {
+                        if(el !== item && el.classList.contains('open')){
+                            const c = el.querySelector('.accordeon_content');
+                            c.style.height = c.scrollHeight + 'px';
+                            requestAnimationFrame(()=>{
+                                c.style.height = '0px';
+                            });
+                            el.classList.remove('open');
+                        }
+                    });
+                }
+                if(isOpen){
+                    content.style.height = content.scrollHeight + 'px';
+                    requestAnimationFrame(()=>{
+                        content.style.height = '0px';
+                    });
+                    item.classList.remove('open');
+                }else{
+                    item.classList.add('open');
+                    content.style.height = '0px';
+                    requestAnimationFrame(()=>{
+                        content.style.height = content.scrollHeight + 'px';
+                    });
+                }
+            });
+        });
+    }
     initAccordion('.benefits-faq-item', false); // Если надо несколько открытых - ставим true
     initAccordion('.address-acrd-item', false); // Если надо несколько открытых - ставим true
 });
