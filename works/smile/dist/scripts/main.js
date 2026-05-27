13ee44d2ecf28594ffe5e8ca74c7f87cedd8dbab
Alex <konyulichalex@gmail.com>
2026-05-27 04:07:44 +0300

    smile desk

--- works/smile/dist/scripts/main.js
+++ works/smile/dist/scripts/main.js
@@ -43,17 +43,121 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     initSlider('.equipment-carousel', {
-        slidesPerView: 4,
-        spaceBetween: 20,
+        loop: true,
+        slidesPerView: "auto",
+        spaceBetween: 8,
+        speed: 500,
+        initialSlide: 0,
+        //centeredSlides: true,
+        //centeredSlidesBounds: true,
+        //slideToClickedSlide: true,
         navigation: {
             prevEl: '.equipment-carousel__prev',
             nextEl: '.equipment-carousel__next',
         },
         breakpoints: {
-            480: { slidesPerView: 1 },
-            768: { slidesPerView: 2.5 },
-            1280: { slidesPerView: 3 },
-            1400: { slidesPerView: 4 }
+            480: { spaceBetween: 20, }
+        },
+        on: {
+        init(swiper) {
+            swiper.el.classList.add('is-initial');
+        },
+
+        touchStart(swiper) {
+            swiper.el.classList.remove('is-initial');
+        },
+
+        slideChangeTransitionStart(swiper) {
+            swiper.el.classList.remove('is-initial');
+        },
+
+        click(swiper) {
+            swiper.el.classList.remove('is-initial');
         }
+    }
     });
+
+    
+
+    // Header
+    const header = document.querySelector('.header');
+    if (!header) return;
+
+    const toggleHeaderClass = () => {
+        if (window.scrollY > 30) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    };
+
+    toggleHeaderClass();
+    window.addEventListener('scroll', toggleHeaderClass);
+    
+
+    // Menu
+    const modal = document.querySelector('.modalmain');
+    const switcher = document.querySelector('.header-menu__switcher');
+    const closeBtn = document.querySelector('.modalmain__close');
+
+    if (!modal || !switcher) return;
+
+    const hasHover = window.matchMedia('(hover: hover)').matches;
+
+    if (hasHover) {
+        switcher.addEventListener('mouseenter', () => {
+            modal.classList.add('open');
+        });
+
+        modal.addEventListener('mouseleave', () => {
+            modal.classList.remove('open');
+        });
+    }
+
+    else {
+        switcher.addEventListener('click', (e) => {
+            e.stopPropagation();
+            modal.classList.toggle('open');
+        });
+
+        document.addEventListener('click', (e) => {
+            if (
+                modal.classList.contains('open') &&
+                !modal.contains(e.target) &&
+                !switcher.contains(e.target)
+            ) {
+                modal.classList.remove('open');
+            }
+        });
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', () => {
+            modal.classList.remove('open');
+        });
+    }
+
+    // Datepicker
+    let dt = new Date;
+    flatpickr.localize(flatpickr.l10ns.ru);
+    let calendar = flatpickr("#datepicker", {
+        dateFormat: "d.m.Y",
+        minDate: "today",
+        //maxDate: dt.setDate(dt.getDate() + 3),
+        // dateFormat: "d.m.Y H:i",
+        //noCalendar: true,
+        // enableTime: true,
+        //time_24hr: true
+        // minDate: new Date(),
+        // maxDate: new Date().fp_incr(7),
+        // mode: "range",
+        locale: {
+            rangeSeparator: " - "
+        },
+    });
+
+    // Custom scrollbar
+    document.querySelectorAll('.custom_scroll').forEach(el => {
+        new SimpleBar(el);
+    })
 });
